bb49d4e10aa0924f6055a297f8ad58375720dfba
David Pérez <david.palmero@cesjuanpablosegundo.es>
2018-04-13 10:31:22 +0200

    v1

---
+++ JavaApplication25/src/javaapplication25/AceptaElRetoEl_pan_de_bodas.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication25;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Usuario 1 DAM
+ */
+public class AceptaElRetoEl_pan_de_bodas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner (System.in);
+        int tamaño;
+        int sol=0;        
+        boolean seguir;       
+   while(seguir=true)
+   {  
+       tamaño=sc.nextInt();
+       
+       if(tamaño==0)
+        {
+            seguir=false;
+        }
+       else
+       {
+           
+      String sillas;
+      
+      sillas=sc.nextLine();
+      
+      
+          
+          
+      }
+      if(sol==0)
+      {System.out.println("ALGUNO NO COME");
+      }
+      if(sol==1)
+      { System.out.println("TODOS COMEN");
+      }
+      
+       }
+      
+   }
+        
+}
+    
